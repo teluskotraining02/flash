440d1fcffbae5376839611d3f356bcd4ae50e99e
Murali Sripada <murali.sripada123@gmail.com>
2018-09-01 15:37:22 +0530

    Initial Commit

---
+++ aaaa/src/aaaa/FirstClass.java
@@ -0,0 +1,11 @@
+package aaaa;
+
+public class FirstClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello World");
+System.out.println("Hello .. How are you doing");
+	}
+
+}
